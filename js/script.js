7f4916c8113a013c0e0b1c4eb3e3aeb25b284f99
MadsKarstoft <mads394@gmail.com>
2019-04-10 10:36:05 +0200

    Tilføjer javascript der kan fjerne elementer fra siden
    Tilføjer en div der hedder "page1". Javascript kan fjerne og tilføje hele div'en, men knappen (næste) fjerner det hele inklusiv knappen.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+  let x = document.getElementById("page1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
