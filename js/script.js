22290fa1d5cf6b2f3416a89cb888e23e40d026a5
Fred768c <47402303+Fred768c@users.noreply.github.com>
2019-04-16 11:35:40 +0200

    Revert "lukket rigtigt"
    This reverts commit 8e06d6f83f3c51e8a8ad9e63870d4fb594579983.

--- js/script.js
+++ js/script.js
@@ -31,6 +31,9 @@ function page3() {
 
 
 
+
+
+
 //back button//
 function backFunction() {
   let x = document.getElementById("page1");
@@ -43,7 +46,6 @@ function backFunction() {
 
   } else {
     x.style.display = "none";
-      
   }
 }
 
